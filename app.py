d8765016bf704dd6a6e5c998c21a04f327aae19c
danp0kes <144858231+danp0kes@users.noreply.github.com>
2023-12-12 14:00:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,185 +1,127 @@
 from dotenv import load_dotenv
 import streamlit as st
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
 import pandas as pd
-import re
-
+from openai import OpenAI
 
 def main():
-    load_dotenv()
-    st.set_page_config(page_title="Ask your PDF")
+    # Create header
     st.header("Ask Goals Database 💬")
+
+    # Load user_goals
+    df = pd.read_csv('user_goals.csv')
+
+    st.sidebar.header("Filter")
+
+    # Set Streamlit elements (sidebar sliders)
+    threshold = st.sidebar.slider("Select Minimum Match Rating", 50, 100, 50)
+    number_matches = st.sidebar.slider("Select Number of Matches", 1, 10, 3)
+    temperature = st.sidebar.slider("Select Temperature", 0.0, 1.0, 0.1)
+    token_output = st.sidebar.slider("Select Token Output Number", 1, 100, 20)
     
-    #load user_goals
-    user_goals = pd.read_csv('user_goals.csv')
-      
-      # load text
-    with open('user_goals.txt') as f:
-        text = f.read()
-      
-      # split into chunks
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=100,
-        chunk_overlap=20,
-        length_function=len
-    )
-    chunks = text_splitter.split_text(text)
-      
-    # create embeddings
-    embeddings = OpenAIEmbeddings()
-    knowledge_base = FAISS.from_texts(chunks, embeddings)
+    # Show database
+    st.sidebar.header("Goals Database")
+    st.sidebar.write(df.drop(columns=['index']))
     
-    # Create threshold as streamlit slider, have two endpoints 0 and 100, start at 50, and step by 1
-    threshold = st.slider("match_rating", 0, 100, (25,75), 1)
     
-    # show user input
-    goal = st.text_input("Input Goal:")
+    # Create subheader that displays question
+    st.subheader("Parameters")
+    
+    # Create elements variable
+    element7 = st.text_input('Enter input 1 here:', value= "Return a number out of 100 that reflects how well the two goals relate.")
+    element8 = st.text_input('Enter input 2 here:', value =  "Do not return words")
+    element9 = st.text_input('Enter input 3 here:', value = 'Only return a number')
+    element10 = st.text_input('Enter input 4 here:', value = '')
+
+
+    # Combine elements that are not empty into one string, seperate by line
+    elements = ""
+    for element in [element7, element8, element9, element10]:
+        if element != "":
+            #seperate by line
+            elements += element + "\n"
+    
+    # Create subheader that displays goal
+    st.subheader("Goal")
+    
+    # Goal input
+    goal = st.text_input('Enter goal here:',value="")  # Replace with actual user input
+    
+    # Create subheader that displays question
+    st.subheader("All Parameters")
+    
+    # Text input question
+    question = (st.text_area('All Inputs:',value= elements,
+            height= 200)
+                )
     if goal != "":
-        docs = knowledge_base.similarity_search(goal)
-        llm = OpenAI()
-        chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-            response = chain.run(input_documents=docs, question=f("""Find the best 3 matches. Give the best one 
-                                 first with the matches ID number along with a match rating out of 100. Give rating 
-                                 levels to an specific integer, not just a factor of 10. Then give me the second best match. 
-                                 Goals may be identical. Goals must be directly inline including the action and the overall benefit. 
-                                 For instance, Don't match 'learning to play an instrument' with 'crafting an instrument' because one 
-                                 is about learning music and the other is about learning how to make an instrument.If any have a rating 
-                                 lower than {threshold}, skip. Only return numbers, Do not return names." + goal"""))
-            print(cb)
         
-        if not re.search(r'\d', response):
-            st.write("No number found.")
-            return
-            
-        else:
-            st.write(f"Answer: {response}")
-            
-            numbers = re.findall(r'\b\d+\b', response)
-            id = numbers[0]
-                
-            if id == "":
-                id= 'No ID Match'
-                    
-            # Create first match header
-            st.header('Match 1')
-
-            # show match id
-            st.write(f"Match ID Number: {id}")
-                
-            # Check if id is not an empty string before converting to int
-            if id and id.strip():
-                try:
-                    id_int = int(id)
-                    if 0 <= id_int < len(user_goals):
-                        matches_names = user_goals.iloc[id_int, 1]
-                        #show match name
-                        matches_names = user_goals.iloc[int(id), 1]
-                        st.write(f"Match Name: {matches_names}")
-                        #select second number from answer for rating
-                        rating = numbers[1]
-                        st.write(f"Match Strength Rating: {rating}")
-                            
-                    else:
-                        st.write("ID is out of range")
-                except ValueError:
-                    st.write("Invalid ID")
-            else:
-                st.write("No ID provided")
-                
-            #show match goal
-            matches_goals = user_goals.iloc[int(id), 2]
-            st.write(f"Match's Goal: {matches_goals}")
-            
-            with get_openai_callback() as cb:
-                reason = chain.run(input_documents=docs, question="Why is" + matches_names + "a good match for" + goal + "?")
-                
-            st.write(f"Reason: {reason}")
-            
-            # Create second match header
-            st.header('Match 2')
-            
-            # show match id
-            st.write(f"Match ID Number: {numbers[2]}")
-            
-            # Save id as variable
-            id = numbers[2]
-            
-            # Check if id is not an empty string before converting to int
-            if id and id.strip():
-                try:
-                    id_int = int(id)
-                    if 0 <= id_int < len(user_goals):
-                        matches_names = user_goals.iloc[id_int, 1]
-                        #show match name
-                        matches_names = user_goals.iloc[int(id), 1]
-                        st.write(f"Match Name: {matches_names}")
-                        #select second number from answer for rating
-
-                        rating = numbers[3]
-                        st.write(f"Match Strength Rating: {rating}")
-                            
-                    else:
-                        st.write("ID is out of range")
-                except ValueError:
-                    st.write("Invalid ID")
-            else:
-                st.write("No ID provided")
-                
-            #show match goal
-            matches_goals = user_goals.iloc[int(id), 2]
-            st.write(f"Match's Goal: {matches_goals}")
-            
-            # Create third match header
-            st.header('Match 3')
-            
-            # show match id
-            st.write(f"Match ID Number: {numbers[4]}")
-            
-            # Save id as variable
-            id = numbers[4]
-            
-            # Check if id is not an empty string before converting to int
-            if id and id.strip():
-                try:
-                    id_int = int(id)
-                    if 0 <= id_int < len(user_goals):
-                        matches_names = user_goals.iloc[id_int, 1]
-                        #show match name
-                        matches_names = user_goals.iloc[int(id), 1]
-                        st.write(f"Match Name: {matches_names}")
-                        #select third number from answer for rating
-                        rating = numbers[5]
-                        st.write(f"Match Strength Rating: {rating}")
-                            
-                    else:
-                        st.write("ID is out of range")
-                except ValueError:
-                    st.write("Invalid ID")
-            else:
-                st.write("No ID provided")
-                
-            #show match goal
-            matches_goals = user_goals.iloc[int(id), 2]
-            st.write(f"Match's Goal: {matches_goals}")
-
-            # show user input
-            user_question3 = st.text_input("Question 3:")
-            if user_question3:
-                st.write(f"Question: {user_question3}")
-                answer3 = qa.run(user_question3)
-                st.write(f"Answer: {answer3}")
-            return
+        # Instantiate the OpenAI client
+        client = OpenAI(api_key="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")
         
-        st.write(response)
+        # Function to compare similarity synchronously between the target sentence and each row in the DataFrame
+        def calculate_similarity(row):
+            prompt_text = f"{question} +Similarity between {goal} and {row['goals']}?"
+            response = client.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "system", "content": "You are a helpful assistant."},
+                            {"role": "user", "content": prompt_text}],
+                temperature= temperature,
+                max_tokens= 20  # Adjust the number of tokens for the desired length of response
+            )   
+            return response.choices[0].message.content
+
+        # Calculate similarity for each row in the DataFrame
+        def process_dataframe(df):
+            similarity_scores = []
+            for index, row in df.iterrows():
+                similarity_score = calculate_similarity(row)
+                similarity_scores.append(similarity_score)
+
+            df['similarity_score'] = similarity_scores
+            print(df)
+
+        # Execute the process_dataframe function
+        process_dataframe(df)
+        
+        st.write(df)
+
+        # Grab just numeric values from similarity_score
+        df['similarity_score'] = df['similarity_score'].str.extract('(\d+)')
+        
+        # Try to Convert 'similarity_score' column to float
+        try:
+            df['similarity_score'] = df['similarity_score'].astype(int)
+        except:
+            pass
+
+        # Display top matches
+        st.header(f"Top {number_matches} Matches")
+        
+        # Convert threshold to int
+        threshold = int(threshold)
+        
+        st.write(df)
+        
+        # Drop na values in similiarity_score column
+        df = df.dropna(subset=['similarity_score'])
+        
+        # Convert similarity_score to int
+        df['similarity_score'] = df['similarity_score'].astype(int)
     
-    
+        # create matches dataframe, limiting similarity score to threshold
+        matches = df['similarity_score'] >= threshold
+        
+        # Order matches by similarity score and limit to number_matches
+        matches = df.sort_values(by=['similarity_score'], ascending=False).head(number_matches)
+        
+        st.write(matches)
+        
+        for i in range(len(matches)):
+            st.header(f"Match {i + 1}")
+            st.write(f"Match ID Number: {str(matches.iloc[i, 0])}")
+            st.write(f"Match Name: {str(matches.iloc[i, 1])}")
+            st.write(f"Match Strength Rating: {str(matches.iloc[i, 3])}")
+            st.write(f"Match Goal: {str(matches.iloc[i, 2])}")
 
 if __name__ == '__main__':
     main()
